1e3c3ed522d8fab73742bde5e8cb0486d4bce19c
Sudipta Banik Trisha <43735253+Sudipta-Trisha@users.noreply.github.com>
2019-08-20 13:54:11 +0600

    Implementation

---
+++ A - Red or Not.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+	 ll n;
+	 string s;
+	 cin>>n>>s;
+
+	 if(n>=3200)
+	 {
+		cout<<s<<endl;
+	 }
+	 else
+		cout<<"red"<<endl;
+
+	 return 0;
+}
