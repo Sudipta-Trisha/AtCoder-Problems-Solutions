62bfe70551299322a6d15f2a989c3ea79f34e06a
Sudipta Banik Trisha <sudiptatrisha.19@gmail.com>
2020-08-30 13:10:16 +0600

    Add files via upload

---
+++ AtCoder Beginner Contest 149/A - Strings/sol.cpp
@@ -0,0 +1,22 @@
+
+/**
+ *    author        : Sudipta Banik Trisha 
+ *    created       : August 30, 2020 12:00 PM
+ *    Problem Name  : A - Strings
+ *    Problem Limit : 2000 ms , 1024 MB
+ *    Problem Url   : https://atcoder.jp/contests/abc149/tasks/abc149_a
+ *    @genarated by : ai-virtual-assistant
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+
+    string s,t;
+    cin >> s >> t;
+
+    cout << t+s << endl;
+    return 0 ;
+}
